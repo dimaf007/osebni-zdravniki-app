e2f1a90ec8e413f0cbc8c9243ef462e3cd74d978
Dmitrii Fedorchuk <dmitrij.slovenija@gmail.com>
2026-07-31 17:16:16 +0200

    Header and footer added. Styles adjusted.

--- front-end/src/AppRouter.tsx
+++ front-end/src/AppRouter.tsx
@@ -2,8 +2,11 @@
 // BrowserRouter skrbi za navigacijo po URL naslovih,
 // Menu prikaže glavni meni,
 // Routes in Route pa povežeta poti s posameznimi stranmi.
+// Poleg tega vsebuje tudi skupni naslov aplikacije in nogo strani,
+// ki se prikažeta na vseh straneh.
 
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import './index.css'
 
 import Menu from './components/Menu'
 import LoginPage from './pages/LoginPage'
@@ -20,21 +23,33 @@ import ResetPasswordPage from './pages/ResetPasswordPage'
 export default function AppRouter() {
   return (
     <BrowserRouter>
-      <Menu />
+      <div className="app-shell">
+        <header className="app-header">
+          <h1 className="app-title">Osebni zdravniki Slovenije</h1>
+        </header>
 
-      <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/subscriptions" element={<MySubscriptionsPage />} />
-        <Route path="/subscriptions/:id" element={<SingleSubscriptionPage />} />
-        <Route path="/subscriptions/:id/edit" element={<EditSubscriptionPage />} />
-        <Route path="/create-subscription" element={<CreateSubscriptionPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/delete-account" element={<DeleteAccountPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
+        <Menu />
+
+        <div className="app-content">
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            <Route path="/subscriptions" element={<MySubscriptionsPage />} />
+            <Route path="/subscriptions/:id" element={<SingleSubscriptionPage />} />
+            <Route path="/subscriptions/:id/edit" element={<EditSubscriptionPage />} />
+            <Route path="/create-subscription" element={<CreateSubscriptionPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/delete-account" element={<DeleteAccountPage />} />
+            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </div>
+
+        <footer className="app-footer">
+          <p>Dmitrii Fedorchuk, 2026</p>
+        </footer>
+      </div>
     </BrowserRouter>
   )
 }
